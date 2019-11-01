b96fd15feeffc331df1ce9c0ad96f07f70698fad
dongbsang <skehdqja@naaver.com>
2019-11-01 20:33:34 +0900

    dongb
    가게등록 틀

---
+++ classmate_shop/www/shop_register.php
@@ -0,0 +1,18 @@
+<?php
+​
+include "dbconn.php";
+​
+  if($userId != null)
+  {
+    $sql = "insert into store (type,shopId, pic, content, event)";
+    $sql = $sql."values('$type', '$shopId', '$pic', '$content', '$event')";
+
+    if($mysqli->query($sql)){
+        echo "<script>alert('가게 등록이 완료되었습니다'); location.href='shop_registration';</script>";
+​
+    }else{
+        echo "<script>alert('등록이 실패하였습니다.');location.href='shop_registration';</script>";
+    }
+  }
+
+?>
