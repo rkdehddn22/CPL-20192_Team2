d1507853a4da51da627dca98c566160877558b92
dongbsang <skehdqja@naaver.com>
2019-11-03 21:59:31 +0900

    dongb
    d

--- classmate_shop/www/shop_register.php
+++ classmate_shop/www/shop_register.php
@@ -4,8 +4,8 @@ include "dbconn.php";
 ​
   if($userId != null)
   {
-    $sql = "insert into store (type,shopId, pic, content, event)";
-    $sql = $sql."values('$type', '$shopId', '$pic', '$content', '$event')";
+    $sql = "insert into store (type,shopId,address, content, event)";
+    $sql = $sql."values('$type', '$shopId', ,'$address', '$content', '$event')";
 
     if($mysqli->query($sql)){
         echo "<script>alert('가게 등록이 완료되었습니다'); location.href='index.html';</script>";
