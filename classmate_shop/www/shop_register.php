e5446d6846da668fab3ed5eba56ecee98fa8a70d
dongbsang <skehdqja@naaver.com>
2019-11-04 16:28:28 +0900

    donb

--- classmate_shop/www/shop_register.php
+++ classmate_shop/www/shop_register.php
@@ -5,11 +5,11 @@
 
 $type=$_POST[type];
 $shopId=$_POST[shopId];
-$postcode=$_POST[postcode];
-$road=$_POST[road];
-$jibun=$_POST[jibun];
-$detail=$_POST[detail];
-$extra=$_POST[extra];
+$sample4_postcode=$_POST[sample4_postcode];
+$sample4_roadAddress=$_POST[sample4_roadAddress];
+$sample4_jibunAddress=$_POST[sample4_jibunAddress];
+$sample4_detailAddress=$_POST[sample4_detailAddress];
+$sample4_extraAddress=$_POST[sample4_extraAddress];
 $content=$_POST[content];
 $event1=$_POST[event1];
 $event3=$_POST[event2];
@@ -18,8 +18,8 @@ $event3=$_POST[event3];
 $regdate=date("YmdHis", time()); //날짜 시간
 $ip=getenv("REMOTE_ADDR"); //ip
 
-$query="insert into shop1(type, shopId, postcode, road, jibun, detail, extra, content, event1, event2, event3)
-values('$type', '$shopId', '$postcode' ,'$road', '$jibun','$detail', '$extra', '$content', '$event1','$event2','$event3')";
+$query="insert into shop1(type, shopId, sample4_postcode, sample4_roadAddress,sample4_jibunAddress, sample4_detailAddress,sample4_extraAddress, content, event1, event2, event3)
+values('$type', '$shopId', '$sample4_postcode' ,'$sample4_roadAddress', '$sample4_jibunAddress','$sample4_detailAddress', '$sample4_extraAddress', '$content', '$event1','$event2','$event3')";
 mysql_query("set names utf8",$connect);
 mysql_query($query,$connect);
 mysql_close; //mysql끝내기
