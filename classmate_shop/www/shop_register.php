bf334a20334a622db3d9ba3e565f46bbdc8a4115
dongbsang <skehdqja@naaver.com>
2019-11-04 15:44:25 +0900

    dongb
    추가

--- classmate_shop/www/shop_register.php
+++ classmate_shop/www/shop_register.php
@@ -1,18 +1,34 @@
-<meta charset="utf-8">
-<?
-   include "../lib/dbconn.php";
-​
-  if($shopId != null)
-  {
-    $sql = "insert into store (type,shopId,address, content, event)";
-    $sql = $sql."values('$type', '$shopId', ,'$address', '$content', '$event')";
+<?header("content-type:text/html; charset=UTF-8");
+/**************************************************************
+***************************************************************
+PHP Script Produced by: 박정원 PJW(Jimmy)
+Email: pjws0321@naver.com / pjws0321@gmail.com
+The purpose of the product : Educational (교육용)
+Notice: 본 스크립트는 교육용도로 만들어 졌습니다.
+Blog  http://blog.naver.com/pjws0321
+****************************************************************
+***************************************************************/
 
-    if($mysqli->query($sql)){
-        echo "<script>alert('가게 등록이 완료되었습니다'); location.href='index.html';</script>";
-​
-    }else{
-        echo "<script>alert('등록이 실패하였습니다.');location.href='index.html';</script>";
-    }
-  }
+   include '../lib/db_connect.php';
+   $connect=dbconn();
 
+$type=$_POST[type];
+$shopId=$_POST[shopId];
+$address=$_POST[address];
+$content=$_POST[content];
+$event=$_POST[event];
+
+$regdate=date("YmdHis", time()); //날짜 시간
+$ip=getenv("REMOTE_ADDR"); //ip
+
+$query="insert into shop(type, shopId, address, content, event)
+values('$type', '$shopId', '$address', '$content', '$event')";
+mysql_query("set names utf8",$connect);
+mysql_query($query,$connect);
+mysql_close; //mysql끝내기
 ?>
+
+<script>
+window. alert('가게 등록이 완료되었습니다.');
+location.href='../index.php';
+</script>
